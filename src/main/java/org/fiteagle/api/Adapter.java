fbe353c34508cbae8c81f507ae8eee0956950f9b
AlexanderWillner <alexander.willner@tu-berlin.de>
2014-02-19 08:03:21 +0100

    added first part of the API

---
+++ src/main/java/org/fiteagle/api/Adapter.java
@@ -0,0 +1,16 @@
+package org.fiteagle.api;
+
+import javax.jms.JMSException;
+import javax.jms.TextMessage;
+
+public abstract class Adapter {
+	public static final String STATUS_STARTED = "started";
+	public static final String STATUS_STOPPED = "stopped";
+	public static final String PROPERTY_STATUS = "status";
+	
+	public static String toDebugString(TextMessage message) throws JMSException {
+		String text = message.getText();
+		String propStatus = message.getStringProperty(Adapter.PROPERTY_STATUS);
+		return "Text: " + text + "; Propery 'status': " + propStatus;
+	}
+}

fe79fc85747e85f48d0dccaec2b2f22ca410f6a2
AlexanderWillner <alexander.willner@tu-berlin.de>
2014-03-04 13:17:10 +0100

    working version with mdb

---
+++ src/main/java/org/fiteagle/api/core/IMessageBus.java
@@ -0,0 +1,7 @@
+package org.fiteagle.api.core;
+
+public interface IMessageBus {
+	public static String TYPE_RESPONSE = "response";
+	public static String TYPE_REQUEST = "request";
+	public static String TYPE_RESULT = "result";
+}
--- src/main/java/org/fiteagle/api/core/IResourceRepository.java
+++ src/main/java/org/fiteagle/api/core/IResourceRepository.java
@@ -4,6 +4,10 @@ public interface IResourceRepository {
 	public static enum Serialization {
 		XML, TTL
 	}
-
+	public static String LIST_RESOURCES = "listResources";
+	public static String TYPE_SERIALIZATION = "serialization";
+	public static String SERIALIZATION_TTL = "ttl";
+	public static String SERIALIZATION_XML = "xml";
+	
 	public String listResources(Serialization type);
 }

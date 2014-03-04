5265539ef5bd9686eae06f42916f0074d6eaf64d
AlexanderWillner <alexander.willner@tu-berlin.de>
2014-03-04 14:18:20 +0100

    refactoring & cleanup

--- src/main/java/org/fiteagle/api/core/IMessageBus.java
+++ src/main/java/org/fiteagle/api/core/IMessageBus.java
@@ -1,7 +1,10 @@
 package org.fiteagle.api.core;
 
 public interface IMessageBus {
-	public static String TYPE_RESPONSE = "response";
-	public static String TYPE_REQUEST = "request";
-	public static String TYPE_RESULT = "result";
+	public static final String TYPE_RESPONSE = "response";
+	public static final String TYPE_REQUEST = "request";
+	public static final String TYPE_RESULT = "result";
+	public static final String TOPIC_CORE = "topic/core";
+	public static final String TOPIC_CORE_NAME = "java:/" + TOPIC_CORE;
+	public static final String TOPIC_ADAPTERS = "java:/topic/adapters";
 }
--- src/main/java/org/fiteagle/api/core/IResourceRepository.java
+++ src/main/java/org/fiteagle/api/core/IResourceRepository.java
@@ -4,10 +4,12 @@ public interface IResourceRepository {
 	public static enum Serialization {
 		XML, TTL
 	}
+
 	public static String LIST_RESOURCES = "listResources";
-	public static String TYPE_SERIALIZATION = "serialization";
+	public static String PROP_SERIALIZATION = "serialization";
 	public static String SERIALIZATION_TTL = "ttl";
 	public static String SERIALIZATION_XML = "xml";
-	
+	public static String MESSAGE_FILTER = IMessageBus.TYPE_REQUEST + " = '" + LIST_RESOURCES + "'";
+
 	public String listResources(Serialization type);
 }

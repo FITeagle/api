d6fb2fe2ad87ce348e7427ee1f19e3793cfaad85
Mitja <mitjanikolaus@gmail.com>
2014-03-07 12:41:13 +0100

    refactoring, fixing access rights for getters for rest interface

--- src/main/java/org/fiteagle/api/UserPublicKey.java
+++ src/main/java/org/fiteagle/api/UserPublicKey.java
@@ -1,29 +1,146 @@
 package org.fiteagle.api;
 
-import java.util.Date;
+import java.io.IOException;
 import java.io.Serializable;
 import java.security.PublicKey;
+import java.util.Date;
+import java.util.regex.Pattern;
 
-public interface UserPublicKey extends Serializable{
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.PrePersist;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.persistence.UniqueConstraint;
+
+
+
+@Entity
+@Table(name="PUBLICKEYS", uniqueConstraints=@UniqueConstraint(columnNames={"owner_username", "description"}))
+public class UserPublicKey implements Serializable{
   
-  public abstract String toString();
+  private static final long serialVersionUID = -374246341434116808L;
   
-  public abstract PublicKey getPublicKey();
+  private final static Pattern KEY_DESCRIPTION_PATTERN = Pattern.compile("[\\w|\\s]+");
   
-  public abstract String getDescription();
+  @Column(length=1024)
+  private PublicKey publicKey;  
   
-  public abstract void setDescription(String description);
+  private String description;
   
-  public abstract String getPublicKeyString();
+  @Id
+  @JoinColumn(name="owner_username")
+  @ManyToOne
+  private User owner;
   
-  public abstract Date getCreated();
+  @Id
+  @Column(length=1024)
+  private String publicKeyString; 
   
-  public abstract User getOwner();
+  @Temporal(TemporalType.TIMESTAMP)
+  private Date created;    
   
-  public abstract void setOwner(User owner);
+   
+  protected UserPublicKey() {
+  } 
   
-  public abstract int hashCode();
+  public UserPublicKey(PublicKey publicKey, String description, String publicKeyString) throws User.NotEnoughAttributesException, IOException {
+    this.publicKey = publicKey;
+    
+    checkPublicKeyString(publicKeyString);
+    this.publicKeyString = publicKeyString;
+    
+    checkDescription(description); 
+    this.description = description;
+  } 
   
-  public abstract boolean equals(Object obj);
+  private void checkDescription(String description) throws User.NotEnoughAttributesException {
+    if(description == null || description.length() == 0){
+      throw new User.NotEnoughAttributesException("no description for public key given");
+    }
+    if(!KEY_DESCRIPTION_PATTERN.matcher(description).matches()){
+      throw new User.InValidAttributeException("empty or invalid key description, only letters, numbers and whitespace is allowed: "+description);
+    }
+  }   
   
+  private void checkPublicKeyString(String publicKeyString) throws User.NotEnoughAttributesException {
+    if(publicKeyString == null || publicKeyString.length() == 0){
+      throw new User.NotEnoughAttributesException("no publicKeyString given");
+    }
+  }   
+  
+  @PrePersist
+  public void updateTimeStamps() {
+    if(created == null) {
+      created = new Date();
+    }
+  }
+  
+  @Override
+  public String toString() {
+    return "PublicKey [publicKey=" + publicKey + ", description=" + description + ", created=" + created + "]";
+  }
+  
+  protected PublicKey getPublicKey() {
+    return publicKey;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public void setDescription(String description) {
+    checkDescription(description);
+    this.description = description;
+  }
+
+  public String getPublicKeyString() {
+    return publicKeyString;
+  }
+
+  public Date getCreated() {
+    return created;
+  }
+
+  protected User getOwner() {
+    return owner;
+  }
+
+  public void setOwner(User owner) {
+    this.owner = (User) owner;
+  }
+
+  @Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((created == null) ? 0 : created.hashCode());
+		result = prime * result
+				+ ((description == null) ? 0 : description.hashCode());
+		result = prime * result
+				+ ((publicKeyString == null) ? 0 : publicKeyString.hashCode());
+		return result;
+	}
+
+  @Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		UserPublicKey other = (UserPublicKey) obj;
+		if (publicKey == null) {
+			if (other.publicKey != null)
+				return false;
+		} else if (!publicKey.equals(other.publicKey))
+			return false;
+		return true;
+	}
+
 }

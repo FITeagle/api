4c376fbd9e5d6f6ddec91d70ffe5829461a4476c
AlexanderWillner <alexander.willner@tu-berlin.de>
2014-03-10 09:39:15 +0100

    added JENA library and more serializations

--- src/main/java/org/fiteagle/api/core/IResourceRepository.java
+++ src/main/java/org/fiteagle/api/core/IResourceRepository.java
@@ -1,15 +1,17 @@
 package org.fiteagle.api.core;
 
 public interface IResourceRepository {
-	public static enum Serialization {
-		XML, TTL
-	}
+	public static final String LIST_RESOURCES = "listResources";
+	public static final String PROP_SERIALIZATION = "serialization";
+	public static final String SERIALIZATION_RDFXML_PLAIN = "RDF/XML";
+	public static final String SERIALIZATION_RDFXML_ABBREV = "RDF/XML-ABBREV";
+	public static final String SERIALIZATION_NTRIPLES = "NT";
+	public static final String SERIALIZATION_TURTLE = "TTL";
+	public static final String SERIALIZATION_N3 = "N3";
+	public static final String SERIALIZATION_JSONLD = "JSON-LD";
+	public static final String SERIALIZATION_RDFJSON = "RDFJSON";
+	public static final String SERIALIZATION_TRIG = "TRIG";
+	public static final String MESSAGE_FILTER = IMessageBus.TYPE_REQUEST + " = '" + LIST_RESOURCES + "'";
 
-	public static String LIST_RESOURCES = "listResources";
-	public static String PROP_SERIALIZATION = "serialization";
-	public static String SERIALIZATION_TTL = "ttl";
-	public static String SERIALIZATION_XML = "xml";
-	public static String MESSAGE_FILTER = IMessageBus.TYPE_REQUEST + " = '" + LIST_RESOURCES + "'";
-
-	public String listResources(Serialization type);
+	public String listResources(String type);
 }

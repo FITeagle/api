1e575a43cb19a14030947f0cda6281907db25f1e
Mitja <mitjanikolaus@gmail.com>
2014-08-06 14:05:35 +0200

    removed isAuthenticated attribute when authorizing requests

--- src/main/java/org/fiteagle/api/core/usermanagement/PolicyEnforcementPoint.java
+++ src/main/java/org/fiteagle/api/core/usermanagement/PolicyEnforcementPoint.java
@@ -10,13 +10,12 @@ public interface PolicyEnforcementPoint {
   public static final String TYPE_PARAMETER_RESOURCE_USERNAME = "parameter_resource_username";
   public static final String TYPE_PARAMETER_ACTION = "parameter_action";
   public static final String TYPE_PARAMETER_ROLE = "parameter_role";
-  public static final String TYPE_PARAMETER_IS_AUTHENTICATED = "parameter_is_authenticated";
   public static final String TYPE_PARAMETER_REQUIRES_ADMIN_RIGHTS = "parameter_requires_admin_rights";
   public static final String TYPE_PARAMETER_REQUIRES_TBOWNER_RIGHTS = "parameter_requires_tbowner_rights";
   
   public static final String TARGET = "policyEnforcementPoint";
   public static final String MESSAGE_FILTER = IMessageBus.TYPE_TARGET + " = '" + TARGET + "'";
   
-  public abstract boolean isRequestAuthorized(String subjectUsername, String resourceUsername, String action, String role, Boolean isAuthenticated, Boolean requiresAdminRights, Boolean requiresTBOwnerRights);
+  public abstract boolean isRequestAuthorized(String subjectUsername, String resourceUsername, String action, String role, Boolean requiresAdminRights, Boolean requiresTBOwnerRights);
   
 }

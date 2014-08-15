72439cda9a3009301c41d6416bb5b6bd610d558a
Mitja <mitjanikolaus@gmail.com>
2014-08-15 17:56:38 +0200

    refactoring: simplified authorization

--- src/main/java/org/fiteagle/api/core/usermanagement/PolicyEnforcementPoint.java
+++ src/main/java/org/fiteagle/api/core/usermanagement/PolicyEnforcementPoint.java
@@ -7,15 +7,12 @@ public interface PolicyEnforcementPoint {
   public static final String IS_REQUEST_AUTHORIZED = "isRequestAuthorized";
 
   public static final String TYPE_PARAMETER_SUBJECT_USERNAME = "parameter_subject_username";
-  public static final String TYPE_PARAMETER_RESOURCE_USERNAME = "parameter_resource_username";
+  public static final String TYPE_PARAMETER_RESOURCE = "parameter_resource_username";
   public static final String TYPE_PARAMETER_ACTION = "parameter_action";
-  public static final String TYPE_PARAMETER_ROLE = "parameter_role";
-  public static final String TYPE_PARAMETER_REQUIRES_ADMIN_RIGHTS = "parameter_requires_admin_rights";
-  public static final String TYPE_PARAMETER_REQUIRES_TBOWNER_RIGHTS = "parameter_requires_tbowner_rights";
   
   public static final String TARGET = "policyEnforcementPoint";
   public static final String MESSAGE_FILTER = IMessageBus.TYPE_TARGET + " = '" + TARGET + "'";
   
-  public abstract boolean isRequestAuthorized(String subjectUsername, String resourceUsername, String action, String role, Boolean requiresAdminRights, Boolean requiresTBOwnerRights);
+  public abstract boolean isRequestAuthorized(String subjectUsername, String resource, String action);
   
 }

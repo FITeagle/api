23f39e7091d2002568187be4a07fb1b96ac3afa9
vju-1011001 <volker.jung@campus.tu-berlin.de>
2014-09-09 12:59:23 +0200

    reading Motor Ontology Model and make it accessable

---
+++ src/main/java/org/fiteagle/api/core/OntologyModels.java
@@ -0,0 +1,36 @@
+package org.fiteagle.api.core;
+
+import com.hp.hpl.jena.rdf.model.Model;
+import com.hp.hpl.jena.rdf.model.ModelFactory;
+import com.hp.hpl.jena.util.FileManager;
+
+import java.io.InputStream;
+
+/**
+ * Created by vju on 9/9/14.
+ */
+public class OntologyModels {
+    static{
+       motorModel = loadModel("ontologies/fiteagle/ontology.ttl", "TURTLE");
+    }
+
+    private static Model motorModel;
+    public static Model getMotorModel(){
+        return motorModel;
+    }
+
+    public static Model loadModel(String filename, String serialization) {
+        Model fiteagle = ModelFactory.createDefaultModel();
+
+        InputStream in2 = FileManager.get().open(filename);
+        if (in2 == null) {
+            throw new IllegalArgumentException("Ontology File: " + filename + " not found");
+        }
+
+        // read the RDF/XML file
+        fiteagle.read(in2, null, serialization);
+
+        return fiteagle;
+    }
+
+}

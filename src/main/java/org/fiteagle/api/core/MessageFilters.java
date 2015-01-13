ca4b1daa0952d9537da61d7a03da59699bbb5f43
mitjanikolaus <mitjanikolaus@gmail.com>
2015-01-13 19:25:50 +0100

    modfied orchestrator filter

--- src/main/java/org/fiteagle/api/core/MessageFilters.java
+++ src/main/java/org/fiteagle/api/core/MessageFilters.java
@@ -4,12 +4,14 @@ public interface MessageFilters {
   
   String FILTER_ORCHESTRATOR = IMessageBus.METHOD_TARGET + " = '" + IMessageBus.TARGET_ORCHESTRATOR + "' "
       + "AND ("+IMessageBus.METHOD_TYPE+" = '"+IMessageBus.TYPE_CONFIGURE+"' "
-      + "OR "+IMessageBus.METHOD_TYPE+" = '"+IMessageBus.TYPE_DELETE+"')";
+      + "OR "+IMessageBus.METHOD_TYPE+" = '"+IMessageBus.TYPE_DELETE+"') "
+      + "OR "+IMessageBus.METHOD_TYPE+" = '"+IMessageBus.TYPE_INFORM+"'";
   
   String FILTER_RESERVATION = IMessageBus.METHOD_TARGET + " = '" + IMessageBus.TARGET_RESERVATION + "' "
       + "AND ("+IMessageBus.METHOD_TYPE+" = '"+IMessageBus.TYPE_CREATE+"' "
       + "OR "+IMessageBus.METHOD_TYPE+" = '"+IMessageBus.TYPE_CONFIGURE+"' "
           + "OR " +IMessageBus.METHOD_TYPE+" = '"+IMessageBus.TYPE_GET+"')";
+  
   String FILTER_RESOURCE_ADAPTER_MANAGER = IMessageBus.METHOD_TARGET + " = '" + IMessageBus.TARGET_RESOURCE_ADAPTER_MANAGER + "'"
       + "AND ("+IMessageBus.METHOD_TYPE+" = '"+IMessageBus.TYPE_CREATE+"' "
       + "OR "+IMessageBus.METHOD_TYPE+" = '"+IMessageBus.TYPE_GET+"' "

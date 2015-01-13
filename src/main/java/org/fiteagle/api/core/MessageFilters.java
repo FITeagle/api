8738af77d2534cde8bd8fc4d7e1a5b731d8fcbc6
dnehls <daniel.nehls@tu-berlin.de>
2015-01-13 18:23:49 +0100

    added GET message filter for reservation module, fixed typo in ontology

--- src/main/java/org/fiteagle/api/core/MessageFilters.java
+++ src/main/java/org/fiteagle/api/core/MessageFilters.java
@@ -8,8 +8,8 @@ public interface MessageFilters {
   
   String FILTER_RESERVATION = IMessageBus.METHOD_TARGET + " = '" + IMessageBus.TARGET_RESERVATION + "' "
       + "AND ("+IMessageBus.METHOD_TYPE+" = '"+IMessageBus.TYPE_CREATE+"' "
-      + "OR "+IMessageBus.METHOD_TYPE+" = '"+IMessageBus.TYPE_CONFIGURE+"')";
-  
+      + "OR "+IMessageBus.METHOD_TYPE+" = '"+IMessageBus.TYPE_CONFIGURE+"' "
+          + "OR " +IMessageBus.METHOD_TYPE+" = '"+IMessageBus.TYPE_GET+"')";
   String FILTER_RESOURCE_ADAPTER_MANAGER = IMessageBus.METHOD_TARGET + " = '" + IMessageBus.TARGET_RESOURCE_ADAPTER_MANAGER + "'"
       + "AND ("+IMessageBus.METHOD_TYPE+" = '"+IMessageBus.TYPE_CREATE+"' "
       + "OR "+IMessageBus.METHOD_TYPE+" = '"+IMessageBus.TYPE_GET+"' "

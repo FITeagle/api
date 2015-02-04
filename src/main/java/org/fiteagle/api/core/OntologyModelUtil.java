6245ff9f7d6551c9b453dafe44f9acd0e9312fb4
mitjanikolaus <mitjanikolaus@gmail.com>
2015-02-04 11:38:57 +0100

    added method to retrieve local namespace

--- src/main/java/org/fiteagle/api/core/OntologyModelUtil.java
+++ src/main/java/org/fiteagle/api/core/OntologyModelUtil.java
@@ -1,24 +1,36 @@
 package org.fiteagle.api.core;
 
+import java.io.InputStream;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 import com.hp.hpl.jena.rdf.model.Model;
 import com.hp.hpl.jena.rdf.model.ModelFactory;
 import com.hp.hpl.jena.util.FileManager;
 
-import java.io.InputStream;
-
 public class OntologyModelUtil {
-
+  
   public static Model loadModel(String filename, String serialization) {
-        Model model = ModelFactory.createDefaultModel();
-
-        InputStream in = FileManager.get().open(filename);
-        if (in == null) {
-            throw new IllegalArgumentException("Ontology File: " + filename + " not found");
-        }
-
-        model.read(in, null, serialization);
-
-        return model;
+    Model model = ModelFactory.createDefaultModel();
+    
+    InputStream in = FileManager.get().open(filename);
+    if (in == null) {
+      throw new IllegalArgumentException("Ontology File: " + filename + " not found");
     }
-
+    
+    model.read(in, null, serialization);
+    
+    return model;
+  }
+  
+  public static String getLocalNamespace() {
+    String hostname = null;
+    try {
+      hostname = InetAddress.getLocalHost().getCanonicalHostName();
+    } catch (UnknownHostException e) {
+      e.printStackTrace();
+    }
+    return "http://".concat(hostname).concat("/");
+  }
+  
 }

a7ab8b7fa8d04d4cb8cfdad2b6a8006513e3577c
mitjanikolaus <mitjanikolaus@gmail.com>
2015-02-04 15:49:44 +0100

    added method to retrieve namespace and localname from uri string

--- src/main/java/org/fiteagle/api/core/OntologyModelUtil.java
+++ src/main/java/org/fiteagle/api/core/OntologyModelUtil.java
@@ -3,6 +3,7 @@ package org.fiteagle.api.core;
 import java.io.InputStream;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
+import java.util.Map;
 
 import com.hp.hpl.jena.rdf.model.Model;
 import com.hp.hpl.jena.rdf.model.ModelFactory;
@@ -31,6 +32,51 @@ public class OntologyModelUtil {
       e.printStackTrace();
     }
     return "http://".concat(hostname).concat("/");
+  }
+  
+  public static String[] getNamespaceAndLocalname(String uri, Map<String, String> prefixes) throws IllegalArgumentException {
+    String[] namespaceAndLocalname = new String[2];
+    if(uri.startsWith("http://")){
+      if(uri.contains("#")){
+        String[] splitted = uri.split("#");
+        if(splitted.length != 2){
+          throw new IllegalArgumentException("Unsupported URI: "+uri);
+        }
+        else{
+          String namespace = splitted[0]+"#";
+          namespaceAndLocalname[0] = namespace;
+          namespaceAndLocalname[1] = splitted[1];
+        }
+      }
+      else{
+        String namespace = uri.substring(0, uri.lastIndexOf("/")+1);
+        String localname = uri.substring(uri.lastIndexOf("/")+1, uri.length());
+        if(namespace.isEmpty() || localname.isEmpty()){
+          throw new IllegalArgumentException("Unsupported URI: "+uri);
+        }
+        namespaceAndLocalname[0] = namespace;
+        namespaceAndLocalname[1] = localname;
+      }
+    }
+    else if(uri.contains(":")){
+      String[] splitted = uri.split(":");
+      if(splitted.length != 2){
+        throw new IllegalArgumentException("Unsupported URI: "+uri);
+      }
+      else{
+        String prefix = splitted[0];
+        String namespace = prefixes.get(prefix);
+        if(namespace == null){
+          throw new IllegalArgumentException("No namespace found for prefix: "+prefix);
+        }
+        namespaceAndLocalname[0] = namespace;
+        namespaceAndLocalname[1] = splitted[1];
+      }
+    }
+    else{
+      throw new IllegalArgumentException("Unsupported instance URI: "+uri);
+    }
+    return namespaceAndLocalname;
   }
   
 }

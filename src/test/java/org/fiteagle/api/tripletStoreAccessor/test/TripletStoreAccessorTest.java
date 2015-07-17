852450df1ce7eb5b5fd0e9d2c262efd58d1e71bd
Alex-O. <wpinf05.ortlieb@gmail.com>
2015-07-17 14:38:39 +0200

    moved TripletStoreAccessor to API

---
+++ src/test/java/org/fiteagle/api/tripletStoreAccessor/test/TripletStoreAccessorTest.java
@@ -0,0 +1,17 @@
+package org.fiteagle.api.tripletStoreAccessor.test;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.fail;
+
+import org.fiteagle.api.core.IMessageBus;
+import org.fiteagle.api.core.MessageUtil;
+import org.fiteagle.api.core.MessageUtil.ParsingException;
+import org.fiteagle.api.tripletStoreAccessor.TripletStoreAccessor.ResourceRepositoryException;
+
+import com.hp.hpl.jena.rdf.model.Model;
+
+public class TripletStoreAccessorTest {
+  
+
+  
+}

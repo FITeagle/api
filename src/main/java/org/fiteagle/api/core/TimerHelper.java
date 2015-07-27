265d8497da1350d98a18af5de88e712ce181a32b
Alex-O. <wpinf05.ortlieb@gmail.com>
2015-07-27 16:43:34 +0200

    optimized for ManagedThreadExecutor methods

--- src/main/java/org/fiteagle/api/core/TimerHelper.java
+++ src/main/java/org/fiteagle/api/core/TimerHelper.java
@@ -1,50 +1,91 @@
 package org.fiteagle.api.core;
 
-import java.util.Timer;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
 import java.util.TimerTask;
 import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import javax.naming.TimeLimitExceededException;
+import javax.annotation.Resource;
+import javax.ejb.Singleton;
+import javax.ejb.Startup;
+import javax.ejb.Timeout;
+import javax.ejb.Timer;
+import javax.ejb.TimerConfig;
+import javax.ejb.TimerService;
+import javax.enterprise.concurrent.ManagedExecutorService;
+import javax.enterprise.concurrent.ManagedThreadFactory;
 
-public class TimerHelper extends TimerTask {
+import org.apache.jena.atlas.web.HttpException;
+import org.fiteagle.api.tripletStoreAccessor.TripletStoreAccessor;
+import org.fiteagle.api.tripletStoreAccessor.TripletStoreAccessor.ResourceRepositoryException;
+
+import com.hp.hpl.jena.shared.NotFoundException;
+
+
+
+@Singleton
+@Startup
+public class TimerHelper{
     private static final Logger LOGGER = Logger.getLogger(TimerHelper.class.getName());
 
-	
-	Callable<Void> task;
-	private Timer timer;
+	private Callable<Void> task;
 	long delay;
+	private int failureCounter = 0;
+	private Timer timer;
+	private Random random = new Random();
+	
+	@Resource
+	private ManagedThreadFactory threadFactory;
+	@Resource
+	private ManagedExecutorService executorService;
+	@Resource
+	private TimerService timerService;
+	
+	Map list = new HashMap<String,Callable<Void>>();
+	ThreadGroup group = new ThreadGroup("timer");
 
-	public TimerHelper(Callable<Void> task) {
-		timer = new Timer();
-		delay = 500;
-		this.task = task;
-		run();
+	public TimerHelper() {
+		// TODO Auto-generated constructor stub
 	}
+	
+	public TimerHelper(Callable<Void> task) {
+		delay = 500;
+	}
+	
+	public void setNewTimer(Callable<Void> task){
+	this.task = task;
+	timerService.createIntervalTimer(0, 5000, new TimerConfig());
+}
 
-	@Override
-	public void run() {
-
-		if (delay < 3600000) {
-
-			try {
-				task.call();
+	@Timeout
+	public void timerMethod(Timer timer) throws Exception {
+		
+		if(failureCounter <10){
+			
+			try{
+				Future<Void> future = executorService.submit(task);
+				future.get();		
+				LOGGER.log(Level.INFO, "Added something to RDF-Database");
+				failureCounter =0;
 				timer.cancel();
-			} catch (Exception e) {
-				delay = delay + delay;
-				timer.schedule(new TimerHelper(task), delay);
-			}
-		} else {
-			try {
-				timer.cancel();
-				throw new TimeLimitExceededException();
-			} catch (TimeLimitExceededException e) {
-				// TODO Auto-generated catch block
-				LOGGER.log(Level.SEVERE,"Tried to write to TripletStore several times, but could not succeed");
-				e.printStackTrace();
-			}
+				}catch(Exception e){
+					failureCounter++;
+					LOGGER.log(Level.WARNING, "Errored while adding something to Database - will try again " + (10 -failureCounter) +" times");
+				}
+
+		}else{
+			timer.cancel();
+			LOGGER.log(Level.SEVERE, "Tried to add something to Database several times, but failed. Please check the OpenRDF-Database");
+
 		}
+		
+
 	}
 
 }

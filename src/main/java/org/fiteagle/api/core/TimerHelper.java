9901789e9fa0020203ef3c0e6270dc1a65e72bc0
Alex-O. <wpinf05.ortlieb@gmail.com>
2015-08-06 15:20:10 +0200

    new try to use callables in TimerHelper

--- src/main/java/org/fiteagle/api/core/TimerHelper.java
+++ src/main/java/org/fiteagle/api/core/TimerHelper.java
@@ -1,68 +1,76 @@
-/*package org.fiteagle.api.core;
+package org.fiteagle.api.core;
 
 import java.util.concurrent.Callable;
 import java.util.concurrent.Future;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import javax.annotation.PostConstruct;
 import javax.annotation.Resource;
 import javax.ejb.SessionContext;
-import javax.ejb.Singleton;
-import javax.ejb.Stateless;
+import javax.ejb.Startup;
 import javax.ejb.Timeout;
 import javax.ejb.Timer;
 import javax.ejb.TimerConfig;
 import javax.ejb.TimerService;
 import javax.enterprise.concurrent.ManagedExecutorService;
-import javax.inject.Inject;
+import javax.enterprise.context.ApplicationScoped;
 
-
-
-public class TimerHelper{
-    private static final Logger LOGGER = Logger.getLogger(TimerHelper.class.getName());
+@Startup
+@ApplicationScoped
+public class TimerHelper {
+	private static final Logger LOGGER = Logger
+			.getLogger(TimerHelper.class.getName());
 
 	private Callable<Void> task;
-	long delay;
 	private int failureCounter = 0;
+
+	public TimerHelper() {
+	}
+
+@PostConstruct
+public void setup(){
+	timerService = sessionContext.getTimerService();
 	
-	
-	@Inject
-	private SessionContext context;
-	@Inject
+	timerService = sessionContext.getTimerService();
+}
+	@Resource
 	private ManagedExecutorService executorService;
 	@Resource
+    SessionContext sessionContext;
 	private TimerService timerService;
-	
-	public void setNewTimer(Callable<Void> task){
-	this.task = task;
-	if(timerService == null){
-		timerService = context.getTimerService();
+
+	public void setNewTimer(Callable<Void> task) {
+		this.task = task;
+
+		timerService.createIntervalTimer(0, 5000, new TimerConfig());
 	}
-	timerService.createIntervalTimer(0, 5000, new TimerConfig());
-}
 
 	@Timeout
 	public void timerMethod(Timer timer) {
-		if(failureCounter <10){
-			
-			try{
-				Future<Void> future = executorService.submit(task);
-				future.get();		
-				LOGGER.log(Level.INFO, "Added something to RDF-Database");
-				failureCounter =0;
-				timer.cancel();
-				}catch(Exception e){
-					failureCounter++;
-					LOGGER.log(Level.WARNING, "Errored while adding something to Database - will try again " + (10 -failureCounter) +" times");
-				}
+		if (failureCounter < 10) {
 
-		}else{
+			try {
+				Future<Void> future = executorService.submit(task);
+				future.get();
+				LOGGER.log(Level.INFO, "Added something to RDF-Database");
+				failureCounter = 0;
+				timer.cancel();
+			} catch (Exception e) {
+				failureCounter++;
+				LOGGER.log(Level.WARNING,
+						"Errored while adding something to Database - will try again "
+								+ (10 - failureCounter) + " times");
+			}
+
+		} else {
 			timer.cancel();
-			LOGGER.log(Level.SEVERE, "Tried to add something to Database several times, but failed. Please check the OpenRDF-Database");
+			LOGGER.log(
+					Level.SEVERE,
+					"Tried to add something to Database several times, but failed. Please check the OpenRDF-Database");
 
 		}
-		
 
 	}
 
-}*/
+}
